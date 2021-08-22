87449024f745b851b1131b9cc87ce01aed6c2851
Lucca Rocha <lucca.rocha@outlook.com>
2021-08-22 14:08:14 -0300

    class 3 - bool, concatenacao

---
+++ bool-concatenacao.dart
@@ -0,0 +1,20 @@
+void main() {
+  //Criando um produto.
+  String produto;
+  produto = "Arroz"; // aspas simples ou duplas
+  print(produto);
+
+  //Quantidade de sacos de arroz.
+  int quantidade = 20;
+
+  // Preço do arroz.
+  double preco = 10.99;
+
+  bool entrega = true;
+
+  produto = "Arroz Branco";
+
+  print("O " + produto + " está");
+  print("O Arroz Branco está");
+  print("O $produto está $preco");
+}
