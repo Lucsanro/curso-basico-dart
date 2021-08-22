2226cc71dacd33f533d8c2bc6f14d6bf72c28c55
Lucca Rocha <lucca.rocha@outlook.com>
2021-08-22 13:54:43 -0300

    class 2 - string, int and double

---
+++ string-int-double.dart
@@ -0,0 +1,15 @@
+void main() {
+  //Criando um produto.
+  String produto;
+  produto = "Arroz"; // aspas simples ou duplas
+
+  //Quantidade de sacos de arroz.
+  int quantidade = 20;
+
+  // Preço do arroz.
+  double preco = 10.99;
+
+  print(produto);
+  print(quantidade);
+  print(preco);
+}
